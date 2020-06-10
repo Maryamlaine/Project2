c712e1be488db2282c039413ae6520a588362ab0
homelaptop <m.tab59@gmail.com>
2020-06-09 19:29:34 -0500

    updating files

--- dashboard/static/js/placeholder1.js
+++
@@ -1,2 +0,0 @@
-d3.select(".flight-routes-d3").append("text").text("Placeholder for d3 visualization")
-    
--- dashboard/static/js/placeholder3.js
+++
@@ -1,2 +0,0 @@
-d3.select(".heatmap-d3").append("text").text("Placeholder for d3 visualization")
-    
